d9822db64a0e4c6cb7b95a86b06e58a96c1518b9
Amer Hero <amherhero@gmail.com>
2020-09-09 11:28:27 +0200

    base architecture of project added

---
+++ src/app/main-home/home/sidebar/novi-unos/novi-unos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novi-unos',
+  templateUrl: './novi-unos.component.html',
+  styleUrls: ['./novi-unos.component.css']
+})
+export class NoviUnosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
