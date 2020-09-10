7c12510fd96846113260ad5b96fef056734b68bf
Elvir9 <ehalilovicc@gmail.com>
2020-09-10 16:32:18 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './main-home/home/home.component';
+import { NarudzbeComponent } from './main-home/home/narudzbe/narudzbe.component';
+import { NoviUnosComponent } from './main-home/home/novi-unos/novi-unos.component';
+import { PostavkeComponent } from './main-home/home/postavke/postavke.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent,
+  // },
+  // TODO: Pokusati sa rijesenjem lazyLoadinga zbog ucitvanja ngModela (ako ovo rijesenje bude radilo sporo)
+  {
+    path: '',
+    component: NarudzbeComponent,
+  },
+  {
+    path: 'novi-unos',
+    component: NoviUnosComponent,
+  },
+  {
+    path: 'postavke',
+    component: PostavkeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
