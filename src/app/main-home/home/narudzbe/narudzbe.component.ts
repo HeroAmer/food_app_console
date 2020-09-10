894c7b6abccf240190a3c33ff7ee62b65cc96eb1
Amer Hero <amherhero@gmail.com>
2020-09-10 18:02:29 +0200

    charts added

--- src/app/main-home/home/narudzbe/narudzbe.component.ts
+++ src/app/main-home/home/narudzbe/narudzbe.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Chart } from 'node_modules/chart.js';
+import { ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-narudzbe',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NarudzbeComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+    var ctx = 'myChart';
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [1, 2, 3, 4, 5, 6, 7],
+    datasets: [{
+        data: [2,8,3,6,3,10],
+        label: "Broj narudzbi",
+        borderColor: "#3e95cd",
+        fill: false
+      },
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Po danima'
+    },
+    scales: {
+      yAxes: [{
+          ticks: {
+              display: false
+          }
+      }]
   }
+  }
+});
+ }
+
 
 }
