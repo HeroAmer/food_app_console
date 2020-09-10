d9ddad9fab81027a3cc8de16c53a15017d51301f
Amer Hero <amherhero@gmail.com>
2020-09-10 13:56:53 +0200

    upload hrane

--- src/app/main-home/home/novi-unos/novi-unos.component.ts
+++ src/app/main-home/home/novi-unos/novi-unos.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { InputDialogComponent } from './input-dialog/input-dialog.component';
 import {MatDialog} from '@angular/material/dialog';
+import {ItemService} from '../../../services/item-service.service';
+import {Hrana} from '../../../models/hrana-unos';
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -9,24 +15,43 @@ import {MatDialog} from '@angular/material/dialog';
   styleUrls: ['./novi-unos.component.css']
 })
 export class NoviUnosComponent implements OnInit {
+  urlTest;
+  hrana: Hrana = {
+    naziv : '',
+    kategorija:'',
+    opis:'',
+    cijena:'',
+    oznaka:'',
+    kolicina:'',
+    imageURL:''
+  }
 
-  constructor(public dialog: MatDialog) {}
+  ref:AngularFireStorageReference;
+  task:AngularFireUploadTask;
+  downloadURL:Observable<string>;
+  constructor(private afStorage:AngularFireStorage, private itemService: ItemService) {}
 
 
   ngOnInit(): void {
   }
 
-  openDialog() {
-    const dialogRef = this.dialog.open(InputDialogComponent,{
+  upload(event){
+    const id = Math.random().toString(36).substring(2);
+    this.ref = this.afStorage.ref(id);
+    this.task = this.ref.put(event.target.files[0]);
+    this.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.ref.getDownloadURL().subscribe(url => {
+          console.log(url);
+          this.urlTest = url; // <-- do what ever you want with the url..
+        });
+      })
+    ).subscribe();
+  }
 
-    });
-
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  };
-
-
+saveFood(){
+  this.hrana.imageURL = this.urlTest;
+  this.itemService.addHrana(this.hrana);
+}
 
 }
--- src/app/services/item-service.service.ts
+++ src/app/services/item-service.service.ts
@@ -7,17 +7,23 @@ import {
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Item } from '../models/item';
+import { Hrana } from '../models/hrana-unos';
 @Injectable({
   providedIn: 'root',
 })
 export class ItemService {
+
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
+
+  hranaCollection: AngularFirestoreCollection<Hrana>;
+  hrana: Observable<Hrana[]>;
 
   constructor(public afs: AngularFirestore) {
     this.itemsCollection = this.afs.collection('items', (ref) =>
       ref.orderBy('description', 'asc')
     );
+
     // this.items = firestore.collection('items').valueChanges();
     this.items = this.itemsCollection.snapshotChanges().map((changes) => {
       return changes.map((a) => {
@@ -26,6 +32,19 @@ export class ItemService {
         return data;
       });
     });
+
+
+
+    this.hranaCollection = this.afs.collection('unos_hrane', (ref) =>
+    ref.orderBy('description', 'asc')
+  );
+  this.hrana = this.itemsCollection.snapshotChanges().map((changes) => {
+    return changes.map((a) => {
+      const data = a.payload.doc.data() as Hrana;
+      data.id = a.payload.doc.id;
+      return data;
+    });
+  });
   }
 
   getItems() {
@@ -34,5 +53,9 @@ export class ItemService {
 
   addItem(item: Item) {
     this.itemsCollection.add(item);
+  }
+
+  addHrana(hrana:Hrana){
+    this.hranaCollection.add(hrana)
   }
 }
