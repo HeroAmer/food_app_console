31ab5a3c3675f84cae4b809c980b093bcfc2098b
Amer Hero <amherhero@gmail.com>
2020-09-11 11:43:32 +0200

    unos dialog almost finished

--- src/app/main-home/home/novi-unos/input-dialog/input-dialog.component.ts
+++ src/app/main-home/home/novi-unos/input-dialog/input-dialog.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ItemService} from '../../../../services/item-service.service';
+import {Hrana} from '../../../../models/hrana-unos';
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-input-dialog',
@@ -7,9 +15,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputDialogComponent implements OnInit {
 
-  constructor() { }
+  urlTest;
+  hrana: Hrana = {
+    naziv : '',
+    kategorija:'',
+    opis:'',
+    cijena:'',
+    oznaka:'',
+    kolicina:'',
+    imageURL:''
+  }
+
+  ref:AngularFireStorageReference;
+  task:AngularFireUploadTask;
+  downloadURL:Observable<string>;
+  constructor(private afStorage:AngularFireStorage, private itemService: ItemService) {}
+
 
   ngOnInit(): void {
   }
+
+  upload(event){
+    const id = Math.random().toString(36).substring(2);
+    this.ref = this.afStorage.ref(id);
+    this.task = this.ref.put(event.target.files[0]);
+    this.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.ref.getDownloadURL().subscribe(url => {
+          console.log(url);
+          this.urlTest = url; // <-- do what ever you want with the url..
+        });
+      })
+    ).subscribe();
+  }
+
+saveFood(){
+  this.hrana.imageURL = this.urlTest;
+  this.itemService.addHrana(this.hrana);
+}
 
 }
--- src/app/main-home/home/novi-unos/novi-unos.component.ts
+++ src/app/main-home/home/novi-unos/novi-unos.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { InputDialogComponent } from './input-dialog/input-dialog.component';
 import {MatDialog} from '@angular/material/dialog';
-import {ItemService} from '../../../services/item-service.service';
-import {Hrana} from '../../../models/hrana-unos';
-import { finalize } from 'rxjs/operators';
-import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
-import { Observable } from 'rxjs';
+// import {ItemService} from '../../../services/item-service.service';
+// import {Hrana} from '../../../models/hrana-unos';
+// import { finalize } from 'rxjs/operators';
+// import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
+// import { Observable } from 'rxjs';
 
 
 
@@ -15,43 +15,54 @@ import { Observable } from 'rxjs';
   styleUrls: ['./novi-unos.component.css']
 })
 export class NoviUnosComponent implements OnInit {
-  urlTest;
-  hrana: Hrana = {
-    naziv : '',
-    kategorija:'',
-    opis:'',
-    cijena:'',
-    oznaka:'',
-    kolicina:'',
-    imageURL:''
+
+  ngOnInit(){
+
   }
+  // urlTest;
+  // hrana: Hrana = {
+  //   naziv : '',
+  //   kategorija:'',
+  //   opis:'',
+  //   cijena:'',
+  //   oznaka:'',
+  //   kolicina:'',
+  //   imageURL:''
+  // }
 
-  ref:AngularFireStorageReference;
-  task:AngularFireUploadTask;
-  downloadURL:Observable<string>;
-  constructor(private afStorage:AngularFireStorage, private itemService: ItemService) {}
+  // ref:AngularFireStorageReference;
+  // task:AngularFireUploadTask;
+  // downloadURL:Observable<string>;
+  // constructor(private afStorage:AngularFireStorage, private itemService: ItemService) {}
+  constructor(public dialog: MatDialog) {}
+  openDialog() {
+    const dialogRef = this.dialog.open(InputDialogComponent);
 
-
-  ngOnInit(): void {
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
-
-  upload(event){
-    const id = Math.random().toString(36).substring(2);
-    this.ref = this.afStorage.ref(id);
-    this.task = this.ref.put(event.target.files[0]);
-    this.task.snapshotChanges().pipe(
-      finalize(() => {
-        this.ref.getDownloadURL().subscribe(url => {
-          console.log(url);
-          this.urlTest = url; // <-- do what ever you want with the url..
-        });
-      })
-    ).subscribe();
-  }
-
-saveFood(){
-  this.hrana.imageURL = this.urlTest;
-  this.itemService.addHrana(this.hrana);
 }
 
-}
+  // ngOnInit(): void {
+  // }
+
+//   upload(event){
+//     const id = Math.random().toString(36).substring(2);
+//     this.ref = this.afStorage.ref(id);
+//     this.task = this.ref.put(event.target.files[0]);
+//     this.task.snapshotChanges().pipe(
+//       finalize(() => {
+//         this.ref.getDownloadURL().subscribe(url => {
+//           console.log(url);
+//           this.urlTest = url; // <-- do what ever you want with the url..
+//         });
+//       })
+//     ).subscribe();
+//   }
+
+// saveFood(){
+//   this.hrana.imageURL = this.urlTest;
+//   this.itemService.addHrana(this.hrana);
+// }
+
