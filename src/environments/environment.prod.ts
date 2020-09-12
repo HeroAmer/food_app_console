6b3f471747dc47aa8803044e4617be8204a8542a
Elvir9 <ehalilovicc@gmail.com>
2020-09-12 10:34:43 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCswrQjdsaMhSx-ZyIsCSuAPYscA1YyV9s',
+    authDomain: 'zeljofoodapp.firebaseapp.com',
+    databaseURL: 'https://zeljofoodapp.firebaseio.com',
+    projectId: 'zeljofoodapp',
+    storageBucket: 'zeljofoodapp.appspot.com',
+    messagingSenderId: '897109079705',
+    appId: '1:897109079705:web:661d985d19273e132cea08',
+  },
 };
